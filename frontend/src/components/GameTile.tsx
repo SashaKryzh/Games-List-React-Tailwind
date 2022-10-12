415569a8a0cc93661988677a28b12f5c34a7b0a9
Denys <dodenisa@gmail.com>
2022-10-12 20:20:42 +0300

    Updated game cover

--- frontend/src/components/GameTile.tsx
+++ frontend/src/components/GameTile.tsx
@@ -2,15 +2,14 @@ import { ReactElement } from "react";
 import { Link } from "react-router-dom";
 
 export default function GameTile(props: {
-  game: { name: string; slug: string, imageUrl?: string };
+  game: { name: string; slug: string, coverUrl?: string };
 }): ReactElement {
-  const url =
-    "https://c.files.bbci.co.uk/E909/production/_112375695_crucible976.jpg";
+  const url = "https://via.placeholder.com/150/000000/FFFFFF/?text=No+Cover+Found";
 
   return (
     <Link to={props.game.slug}>
       <div className="relative aspect-[3/4] rounded-sm flex">
-        <img src={url} className="object-cover min-h-full" alt="" />
+        <img src={props.game.coverUrl ?? url} className="object-cover min-h-full" alt="" />
         <div className="absolute top-0 left-0 min-h-full min-w-full hover:bg-black hover:bg-opacity-25 transition-all bg-gradient-to-t from-black opacity-80" />
         <div className="absolute bottom-4 left-4 ">
           <span className="text-2xl text-white">{props.game.name}</span>
