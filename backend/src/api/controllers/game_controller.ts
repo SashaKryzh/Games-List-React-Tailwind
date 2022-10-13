3cafa13c50fb69a8c6411f99b2f70c606d06094f
Oleksandr Kryzhanovskyi <sashakryzh@gmail.com>
2022-10-13 19:41:42 +0200

    pagination on backend

--- backend/src/api/controllers/game_controller.ts
+++ backend/src/api/controllers/game_controller.ts
@@ -2,43 +2,57 @@ import { Game } from "@app/models";
 import IGDBProxy from "../proxies/igdb_proxy.js";
 
 export default class GameController {
+  static async getGames(page: number): Promise<Game[]> {
+    const offset = (page - 1) * 20;
 
-    static async getGames(): Promise<Game[]> {
-        const response = await IGDBProxy.post("/games", "fields id, slug, name, cover.url; limit 20;");
-        if (response.status !== 200) {
-            return [];
-        }
-        return response.data.map(this.mapResponseToGame);
+    const response = await IGDBProxy.post(
+      "/games",
+      `fields id, slug, name, cover.url; limit 20; offset ${offset};`
+    );
+    if (response.status !== 200) {
+      return [];
     }
+    return response.data.map(this.mapResponseToGame);
+  }
 
-    static async getGamesByName(name: string): Promise<Game[]> {
-        const response = await IGDBProxy.post("/games", `fields id, slug, name, cover.url, summary, genres.name, screenshots.url; limit 20; search "${name}";`);
-        if (response.status !== 200) {
-            return [];
-        }
-        // TODO: populate with company
-        return response.data.map(this.mapResponseToGame);
-    }
+  static async getGamesByName(name: string, page: number): Promise<Game[]> {
+    const offset = (page - 1) * 20;
 
-    static async getGamesBySlug(slug: string): Promise<Game[]> {
-        const response = await IGDBProxy.post("/games", `fields id, slug, name, cover.url, summary, genres.name, screenshots.url; limit 20; where slug = "${slug}";`);
-        if (response.status !== 200) {
-            return [];
-        }
-        // TODO: populate with company
-        return response.data.map(this.mapResponseToGame);
+    const response = await IGDBProxy.post(
+      "/games",
+      `fields id, slug, name, cover.url, summary, genres.name, screenshots.url; limit 20; offset ${offset}; search "${name}";`
+    );
+    if (response.status !== 200) {
+      return [];
     }
+    // TODO: populate with company
+    return response.data.map(this.mapResponseToGame);
+  }
 
-    private static mapResponseToGame(response: any): Game {
-        return {
-            id: response.id,
-            name: response.name,
-            slug: response.slug,
-            coverUrl: response.cover?.url,
-            summary: response.summary,
-            genres: response.genres?.map((genre: any) => genre.name),
-            screenshots: response.screenshots?.map((screenshot: any) => screenshot.url),
-            company: response.company?.name,
-        };
+  static async getGamesBySlug(slug: string): Promise<Game[]> {
+    const response = await IGDBProxy.post(
+      "/games",
+      `fields id, slug, name, cover.url, summary, genres.name, screenshots.url; limit 20; where slug = "${slug}";`
+    );
+    if (response.status !== 200) {
+      return [];
     }
+    // TODO: populate with company
+    return response.data.map(this.mapResponseToGame);
+  }
+
+  private static mapResponseToGame(response: any): Game {
+    return {
+      id: response.id,
+      name: response.name,
+      slug: response.slug,
+      coverUrl: response.cover?.url,
+      summary: response.summary,
+      genres: response.genres?.map((genre: any) => genre.name),
+      screenshots: response.screenshots?.map(
+        (screenshot: any) => screenshot.url
+      ),
+      company: response.company?.name,
+    };
+  }
 }
--- backend/src/api/routes/game_routes.ts
+++ backend/src/api/routes/game_routes.ts
@@ -3,10 +3,18 @@ import GameController from "../controllers/game_controller.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => res.send(await GameController.getGames()));
+router.get("/", async (req, res) =>
+  res.send(await GameController.getGames(Number(req.query.page)))
+);
 
-router.get("/name/:name", async (req, res) => res.send(await GameController.getGamesByName(req.params.name)));
+router.get("/name/:name", async (req, res) =>
+  res.send(
+    await GameController.getGamesByName(req.params.name, Number(req.query.page))
+  )
+);
 
-router.get("/slug/:slug", async (req, res) => res.send(await GameController.getGamesBySlug(req.params.slug)));
+router.get("/slug/:slug", async (req, res) =>
+  res.send(await GameController.getGamesBySlug(req.params.slug))
+);
 
 export default router;
